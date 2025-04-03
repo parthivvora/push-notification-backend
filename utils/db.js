b77f7d740c686bdc3eeb39bbf55414a3b082efb9
parthivvora <v.parthiv004@gmail.com>
2025-04-03 22:22:46 +0530

    Simplify MongoDB connection options in db.js

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 exports.connectToDb = () => {
   mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 };
